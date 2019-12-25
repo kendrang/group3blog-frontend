18fc3c5a5b2bb0d4f2a8b357ea6e7419b346e23d
John Tokhi <56596400+tokhij@users.noreply.github.com>
2019-12-25 17:24:35 -0500

    done comment.component.ts

--- src/app/add-comment/add-comment.component.ts
+++ src/app/add-comment/add-comment.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
-import {AddCommentService} from "../add-comment.service";
+import {FormControl, FormGroup} from "@angular/forms";
 import {CommentPayload} from "./comment-payload";
+import {AddCommentService} from "../add-comment.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-add-comment',
@@ -9,22 +10,32 @@ import {CommentPayload} from "./comment-payload";
   styleUrls: ['./add-comment.component.css']
 })
 export class AddCommentComponent implements OnInit {
-  comment: CommentPayload;
-  permaLink: number;
 
-  constructor(private router: ActivatedRoute, private commentService: AddCommentService) {
+  addCommentForm: FormGroup;
+  commentPayload: CommentPayload;
+  body = new FormControl('');
+
+  constructor(private addcommentService: AddCommentService, private router: Router) {
+    this.addCommentForm = new FormGroup({
+      body: this.body
+    });
+    this.commentPayload = {
+      postId: '',
+      username: '',
+      content: '',
+      commentId: ''
+    };
   }
 
   ngOnInit() {
-    this.router.params.subscribe(params => {
-      this.permaLink = params['commentId'];
-    });
-    this.commentService.getComment(this.permaLink).subscribe((data: CommentPayload) => {
-      this.comment = data;
-      console.log('data');
-    }, (err: any) => {
+  }
+
+  addComment() {
+    this.commentPayload.content = this.addCommentForm.get('body').value;
+    this.addcommentService.addComment(this.commentPayload).subscribe(data => {
+      this.router.navigateByUrl('/');
+    }, error => {
       console.log('Failure Response');
     });
   }
-
 }
--- src/app/comment/comment.component.ts
+++ src/app/comment/comment.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {CommentPayload} from "../add-comment/comment-payload";
+import {ActivatedRoute} from "@angular/router";
+import {AddCommentService} from "../add-comment.service";
 
 @Component({
   selector: 'app-comment',
@@ -10,9 +12,19 @@ export class CommentComponent implements OnInit {
   comment: CommentPayload;
   permaLink: number;
 
-  constructor() { }
+  constructor(private router: ActivatedRoute, private commentService: AddCommentService) {
+  }
 
   ngOnInit() {
+    this.router.params.subscribe(params => {
+      this.permaLink = params['commentId'];
+    });
+    this.commentService.getComment(this.permaLink).subscribe((data: CommentPayload) => {
+      this.comment = data;
+      console.log('data');
+    }, (err: any) => {
+      console.log('Failure Response');
+    });
   }
 
 }
